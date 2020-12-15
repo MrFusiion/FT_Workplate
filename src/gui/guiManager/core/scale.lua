cfb970720f856c17b8b8e7841a6d0fd8dafd0378
MrFusiion <71613276+MrFusiion@users.noreply.github.com>
2020-12-15 04:15:13 +0100

    created the scale module for the core of the player gui

--- src/gui/guiManager/core/scale.lua
+++ src/gui/guiManager/core/scale.lua
@@ -5,6 +5,31 @@ local playerGui = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui
 
 local scale = {}
 
+function scale:get()
+    local screenSize = game.Workspace.CurrentCamera.ViewportSize
+    if UIS.TouchEnabled and playerGui.CurrentScreenOrientation
+        == Enum.ScreenOrientation.Portrait then
 
+        playerGui.ScreenOrientation = Enum.ScreenOrientation.LandScapeSensor
+        screenSize = Vector2.new(screenSize.Y, screenSize.X)
+    end
+    return math.min(screenSize.X / DEFAULT_RES.X, screenSize.Y / DEFAULT_RES.Y)
+end
+
+function scale:getModifier()
+    if UIS.TouchEnabled then
+        return 1.3
+    else
+        return 1
+    end
+end
+
+function scale:getOffset(offset)
+    return math.floor(self:get() * offset + .5) * self:getModifier()
+end
+
+function scale:getTextSize(size)
+    return math.floor(self:get() * size + .5) * self:getModifier()
+end
 
 local scale
